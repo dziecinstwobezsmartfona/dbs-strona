881ec8324619599c57c658101cdba906c033eb7b
dziecinstwobezsmartfona <maciej.piskorski@dziecinstwobezsmartfona.pl>
2025-12-01 14:27:23 +0100

    Ukończona strona Rozwiązanie

--- app/rozwiazanie/page.tsx
+++ app/rozwiazanie/page.tsx
@@ -50,27 +50,87 @@ export default function NaszaMisja() {
                 </div>
             </section>
             <section className="flex flex-col bg-(--background) rounded-3xl items-center justify-center w-3/4 mx-auto mb-16">
-                <div className="flex flex-col w-full p-8 md:p-16 text-center text-3xl md:text-5xl/16 font-title">
-                    <div className="flex flex-col w-full md:w-3/4 mx-auto items-center justify-center text-center">
+                <div className="flex flex-col w-full text-center text-3xl md:text-5xl/16 font-title">
+                    <div className="flex flex-col w-full md:w-3/4 mx-auto items-center justify-center text-center p-8 md:p-16">
                         <p className="text-3xl md:text-5xl/16 font-title mb-12">Nie jesteśmy przeciwnikami technologii, jesteśmy <i>zwolennikami dobrego dzieciństwa</i></p>
                         <p className="text-lg md:text-xl mb-6 font-sans">Wiemy, że technologia ułatwia wiele obszarów życia człowieka i przyczynia się do rozwoju ludzkości. Ważne jest to, jak z niej korzystamy. Chcemy, aby nasze dzieci dorastały z przekonaniem, że technologia jest narzędziem, a nie czymś, od czego zależy ich samopoczucie.</p>
                         <p className="text-lg md:text-xl mb-6 font-sans">Wierzymy w stopniowe, rozsądne budowanie kompetencji cyfrowych i dawanie dzieciom przestrzeni na to, by mogły po prostu być dziećmi. Bo świat online i tak stanie się kiedyś częścią ich życia.</p>
                         <p className="text-lg md:text-xl mb-12 font-sans">Dzieciństwo Bez Smartfona to droga "złotego środka", równowagi w zdrowym korzystaniu z technologii, a nie zakazów.</p>
-                        <p className="text-lg md:text-xl mb-6 font-sans font-bold">Poniżej przedstawiamy model, który sprawdza się u wielu rodzin:</p>
+                        <p className="text-lg md:text-xl font-sans font-bold">Poniżej przedstawiamy model, który sprawdza się u wielu rodzin:</p>
                     </div>
-                    <div className="w-full p-4"><div className="w-full border-b border-gray-300"></div></div>
-                    <div className="flex flex-col md:flex-row w-full items-center justify-center p-8 md:p-16">
-                        <div className="flex flex-col">
+                    <div className="w-full px-8 md:px-16"><div className="w-full border-b border-gray-300"></div></div>
+                    <div className="flex flex-col md:flex-row w-full items-center md:items-start justify-between p-8 md:p-16">
+                        {/* Wiek 0-11 */}
+                        <div className="flex flex-col items-center justify-start w-full mb-12 px-4">
                             <Image
                                 src="/images/solution-no-phone.webp"
                                 alt=""
                                 width={100}
                                 height={100}
+                                className="mb-6"
                             />
-                            <Tag className="bg-black/10 text-foreground mb-12">0 - 11 lat</Tag>
-                            <p className="text-xl md:text-3xl font-title mb-8">Bez telefonu</p>
+                            <Tag className="bg-(--main-accent) text-foreground mb-6"><b>Wiek 0-11 lat</b></Tag>
+                            <p className="text-2xl md:text-4xl font-title mb-6">Bez telefonu</p>
+                            <p className="text-base md:text-lg font-sans">Nie mówimy „bez internetu” lub „zupełnie bez ekranu”, ale generalnie dzieci w tym wieku nie potrzebują własnych telefonów.</p>
+                        </div>
+                        {/* Wiek 11-14 */}
+                        <div className="flex flex-col items-center justify-start w-full mb-12 px-4">
+                            <Image
+                                src="/images/solution-first-phone.webp"
+                                alt=""
+                                width={100}
+                                height={100}
+                                className="mb-6"
+                            />
+                            <Tag className="bg-(--main-accent) text-foreground mb-6"><b>Wiek 11-14 lat</b></Tag>
+                            <p className="text-2xl md:text-4xl font-title mb-6">Prosty telefon</p>
+                            <p className="text-base md:text-lg font-sans">W tym wieku można dać dziecku prosty telefon - bez mediów społecznościowych i bez przeglądarki internetowej. Dostęp do internetu może mieć na domowym laptopie lub innych wspólnych urządzeniach, z których korzysta cała rodzina.</p>
+                        </div>
+                        {/* Wiek 14-16 */}
+                        <div className="flex flex-col items-center justify-start w-full mb-12 px-4">
+                            <Image
+                                src="/images/solution-restricted-smartphone.webp"
+                                alt=""
+                                width={100}
+                                height={100}
+                                className="mb-6"
+                            />
+                            <Tag className="bg-(--main-accent) text-foreground mb-6"><b>Wiek 14-16 lat</b></Tag>
+                            <p className="text-2xl md:text-4xl font-title mb-6">Wsparcie i nadzór</p>
+                            <p className="text-base md:text-lg font-sans">Na tym etapie można przekazać dziecku smartfon, ale z ustawioną kontrolą rodzicielską oraz jasnymi zasadami dotyczącymi tego, z jakich aplikacji może korzystać i jak długo każdego dnia.</p>
+                        </div>
+                        {/* Wiek 16+ */}
+                        <div className="flex flex-col items-center justify-start w-full mb-12 px-4">
+                            <Image
+                                src="/images/solution-smartphone.webp"
+                                alt=""
+                                width={100}
+                                height={100}
+                                className="mb-6"
+                            />
+                            <Tag className="bg-(--main-accent) text-foreground mb-6"><b>Wiek 16+ lat</b></Tag>
+                            <p className="text-2xl md:text-4xl font-title mb-6">Pierwszy smartfon</p>
+                            <p className="text-base md:text-lg font-sans">Jeśli uznasz, że twój nastolatek dobrze radzi sobie z codzienną obecnością internetu i mediów społecznościowych, pozwól mu swobodnie korzystać ze smartfona. Każdy nastolatek jest inny i to ty jako jego rodzic będziesz wiedział, czy jest już na to gotowy.</p>
                         </div>
                     </div>
+                </div>
+            </section>
+            <section className="flex flex-col md:flex-row bg-white rounded-3xl items-center md:items-start justify-between w-3/4 mx-auto mb-32">
+                <div className="text-left order-2 md:order-1 w-full md:pr-32">
+                    <p className="text-3xl md:text-5xl/16 font-title pb-8">Pamiętaj – każdy ma tu swoją rolę</p>
+                    <p className="text-base md:text-lg font-sans pb-8">Każdy z nas współtworzy przyszłość kolejnych pokoleń: rodzice, nauczyciele, pracownicy ochrony zdrowia, dziadkowie, osoby z życia publicznego, a także sami młodzi ludzie.</p>
+                    <p className="text-base md:text-lg font-sans pb-8">Razem możemy odrzucić fałszywe przekonanie, że dzieciństwo musi odbywać się w towarzystwie ekranów i algorytmów.</p>
+                    <p className="text-base md:text-lg font-sans font-bold">Im więcej z nas włączy się w ten ruch, tym szybciej nasze dzieci odzyskają radosne dzieciństwo.</p>
+                </div>
+                <div className="order-1 md:order-2 w-full mb-12 md:mb-0 md:w-1/2">
+                    <Image
+                        src="/images/role-to-play2.jpg"
+                        alt=""
+                        width={500}
+                        height={500}
+                        className="rounded-3xl w-full h-auto"
+                        sizes="(max-width: 500px) 100vw, 50vw"
+                    />
                 </div>
             </section>
         </main>
